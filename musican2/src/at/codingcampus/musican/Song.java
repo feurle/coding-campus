a476c8fd1a4122abd18208b33f336a638259cecd
Daniel Feurle <feurle@gmail.com>
2020-04-04 12:18:57 +0200

    add sufas version

---
+++ musican2/src/at/codingcampus/musican/Song.java
@@ -0,0 +1,13 @@
+package at.codingcampus.musican;
+
+public class Song {
+    private String name;
+
+    public Song(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
